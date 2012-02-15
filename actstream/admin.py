12de5b36084c4422ff8248af889974bebeaeb58f
Markus Kaiserswerth <github@sensun.org>
2012-02-15 21:19:15 +0100

    Activity stream: add filtering by 'verb' to the admin

--- actstream/admin.py
+++ actstream/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from actstream.models import Action
 
+
 class ActionAdmin(admin.ModelAdmin):
     date_hierarchy = 'timestamp'
     list_display = ('__unicode__', 'actor', 'verb', 'target')
     list_editable = ('verb', )
-    list_filter = ('timestamp', )
+    list_filter = ('timestamp', 'verb')
 
 admin.site.register(Action, ActionAdmin)
